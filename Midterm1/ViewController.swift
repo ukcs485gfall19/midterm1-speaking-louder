051021871b3a0d40a1124fbbf1cf622a29bdc64e
Nathaniel Church <ndchurch@roadrunner.com>
2019-09-17 12:30:14 -0400

    Initial Commit

---
+++ Midterm1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Midterm1
+//
+//  Created by Nathan Church on 9/17/19.
+//  Copyright © 2019 Nathan Church. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
