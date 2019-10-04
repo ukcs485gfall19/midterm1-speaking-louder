686bc55e4b2b0d5dab175e6a3f9b5b216075ce6b
Damon Caskey <dc@caskeys.com>
2019-10-04 16:13:56 -0400

    Units and unit calculations now rolled into enum.

--- Midterm1/unit.swift
+++ Midterm1/unit.swift
@@ -2,34 +2,53 @@
 //  unit.swift
 //  Geocentric
 //
-//  Created by user158055 on 10/4/19.
-//  Copyright © 2019 Nathan Church. All rights reserved.
+//  Created by Caskey, Damon V. on 10/4/19.
+//  Copyright © 2019 Team Speaking Louder. All rights reserved.
 //
 
 import Foundation
 
-struct UnitMultiplier {
-    static let meter     = 1.0
-    static let kilometer = 1000.0
-    static let foot      = 0.3048
-    static let mile      = 1609.34
-}
-
-// Caskey, Damon V
+// Caskey, Damon V.
 // 2019-10-04
 //
-// Calculate units based on user selection.
-func checkUnit(Unit: String, size: Double) -> Double {
-    switch Unit {
-    case "meters":
-        return size * UnitMultiplier.meter
-    case "feet":
-        return size * UnitMultiplier.foot
-    case "miles":
-        return size * UnitMultiplier.mile
-    case "kilometers":
-        return size * UnitMultiplier.kilometer
-    default:
-        return size
+// Basic structure for distance units.
+enum UnitList {
+    case meter
+    case kilometer
+    case foot
+    case mile
+    
+    // User friendly labels.
+    var Label:String{
+        switch self{
+        case .foot: return "Feet"
+        case .kilometer: return "Kilometers"
+        case .meter: return "Meters"
+        case .mile: return "Miles"
+        }
+    }
+    
+    // Calculation multipliers (vs. meters).
+    var Mutiplier:Double{
+        switch self{
+        case .foot: return 0.3048
+        case .kilometer: return 1000.0
+        case .meter: return 1.0
+        case .mile: return 1609.34
+        }
+    }
+    
+    // Calculate distance based on selected unit.
+    func ApplyUnit(unit: UnitList, size: Double) ->Double{
+        switch unit {
+        case .foot:
+            return size * UnitList.foot.Mutiplier
+        case .kilometer:
+            return size * UnitList.kilometer.Mutiplier
+        case .meter:
+            return size * UnitList.meter.Mutiplier
+        case .mile:
+            return size * UnitList.mile.Mutiplier
+        }
     }
 }
